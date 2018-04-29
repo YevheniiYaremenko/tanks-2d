2dacad686b0c49911b95d00e570eb3c334368a61
Yevhenii Yaremenko <jekayarema24@ukr.net>
2018-04-30 00:22:14 +0300

    implement laser shooting

--- Assets/Game/Scripts/Weapon/Laser.cs
+++ Assets/Game/Scripts/Weapon/Laser.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace Game
 {
@@ -8,9 +9,23 @@ namespace Game
 	{
 		public override void Shoot()
 		{
-			base.Shoot();
-			
-			//TODO
+            if (lastReloadTime + reloadDuration > Time.time)
+            {
+                return;
+            }
+            base.Shoot();
+
+            var hits = Physics2D.RaycastAll(startShootPoint.position, startShootPoint.up)
+                .Where(h => h.collider.GetComponent<Tank>() == null)
+                .OrderBy(h => h.distance)
+                .ToArray();
+            foreach(var hit in hits)
+            {
+                if (hitEffect != null)
+                {
+                    Instantiate(hitEffect, hit.point, Quaternion.identity);
+                }
+            }
 		}
 	}
 }
