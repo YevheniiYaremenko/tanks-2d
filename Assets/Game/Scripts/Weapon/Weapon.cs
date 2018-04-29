8936434aec9abee2ca08a5b5afdd89577d828035
Yevhenii Yaremenko <jekayarema24@ukr.net>
2018-04-29 17:32:07 +0300

    add tank and weapon prefabs

--- Assets/Game/Scripts/Weapon/Weapon.cs
+++ Assets/Game/Scripts/Weapon/Weapon.cs
@@ -6,6 +6,7 @@ namespace Game
 {
     public abstract class Weapon : MonoBehaviour
     {
+      [SerializeField] protected Transform startShootPoint;
       [SerializeField] protected float damage;
 
       public abstract void Shoot();
