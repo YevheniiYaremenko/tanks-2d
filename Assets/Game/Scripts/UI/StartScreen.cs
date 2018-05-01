dc9b8dc86b4b5180f8f97cb30436fc0fd2316b1f
Yevhenii Yaremenko <jekayarema24@ukr.net>
2018-05-01 23:59:03 +0300

    title formatting

--- Assets/Game/Scripts/UI/StartScreen.cs
+++ Assets/Game/Scripts/UI/StartScreen.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 namespace Game.UI
 {
@@ -14,7 +15,16 @@ namespace Game.UI
             foreach(var type in types)
             {
                 var tankButton = Instantiate(tankSelectionButton, tankSelectionButton.transform.parent);
-                tankButton.GetComponentInChildren<Text>().text = type.ToString();
+
+                var title = type.ToString();
+                title = System.IO.Path.GetExtension(title);
+                title = title.Remove(0, 1);
+                foreach(var ch in title.Where(x => x >= 'A' && x <= 'Z').Distinct())
+                {
+                    title = title.Replace(ch.ToString(), " " + ch);
+                }
+
+                tankButton.GetComponentInChildren<Text>().text = title;
                 var t = type;
                 tankButton.onClick.AddListener(() => onTankSelected(t));
                 tankButton.gameObject.SetActive(true);
